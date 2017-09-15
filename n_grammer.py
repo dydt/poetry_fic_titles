ec19d05f401a269e1a3a44ad5384b3eb262466dc
Diyang <diyang.tang@gmail.com>
2017-09-14 19:34:23 -0700

    Remove all word frequency logic.
    It hasn't been possible to narrow down the n-grams to a sensible list (based on
    what I've seen work as fic titles) using word frequencies and heuristics, so
    I'm just going collect big corpora and filter out any n-gram that occur there.

--- n_grammer.py
+++ n_grammer.py
@@ -1,11 +1,9 @@
 from nltk import ngrams
-from nltk.corpus import stopwords
+from nltk.corpus import stopwords, brown
 from nltk.stem import SnowballStemmer, WordNetLemmatizer
 from collections import Counter, defaultdict
 import re
 import os
-import csv
-import math
 
 class PoemGrammer(object):
 
@@ -13,29 +11,11 @@ class PoemGrammer(object):
         self.stemmer = SnowballStemmer("english")
         self.lemmatizer = WordNetLemmatizer()
         self.stopwords = set(stopwords.words('english'))
-        self.frequencies = self.word_frequencies('wordfrequencyinfo.csv', 
-                                                 4500)
+        self.corpus = brown
+
         self.n_grams = self.all_ngrams(poems_folder)
-        self.viable_grams = self.filter_frequencies(threshold)
-
-    def word_frequencies(self, freq_file, default_val):
-        frequencies = defaultdict(lambda: default_val)
-        with open(freq_file, 'rb') as words:
-            freq_csv = csv.DictReader(words)
-            for row in freq_csv:
-                key = self.stemmer.stem(self.lemmatizer.lemmatize(row['Word']))
-                frequencies[key] = float(row['Frequency'])
-        return frequencies
-
-    def compute_frequencies(self, n_gram):
-        total = 0
-        for word in n_gram: 
-            if word in self.stopwords:
-                continue
-            stemmed_word = self.stemmer.stem(self.lemmatizer.lemmatize(word, pos='v'))
-            score = 100/math.sqrt(self.frequencies[stemmed_word])
-            total += score
-        return total
+        self.corpus_grams = self.corpus_grammer()
+        self.viable_grams = self.filter_ngrams()
 
     def prepare_poem(self, f):
         poem = open(f, 'r').read()
@@ -52,10 +32,22 @@ class PoemGrammer(object):
         
         # Filter out n_grams that appear more than once
         # and ones with a stopword as last word
-        singlets = [k for k, v in all_poem_ngrams.items() 
-                        if v == 1 and k[-1] not in self.stopwords]
+        singlets = set([k for k, v in all_poem_ngrams.items()
+                        if v == 1 and k[-1] not in self.stopwords])
 
         return singlets
+
+    def corpus_grammer(self):
+        ngrams_counter = Counter()
+        for sentence in self.corpus.sents():
+            for i in range(3, 11):
+                sentence = ' '.join(sentence)
+                sentence = re.sub(r'[^\w\s\']', '', sentence)
+                sentence = re.sub(r'\d', '', sentence)
+                sentence = sentence.lower()
+                grams = ngrams(sentence.split(), i)
+                ngrams_counter.update(list(grams))
+        return ngrams_counter
 
     def extract_from_text(self, sentences):
         ngrams_counter = Counter()
@@ -68,15 +60,12 @@ class PoemGrammer(object):
                 ngrams_counter.update(grams)
         return ngrams_counter
 
-    def filter_frequencies(self, threshold):
-        return set(filter(lambda x: self.compute_frequencies(x) > threshold, self.n_grams))
+    def filter_ngrams(self):
+        return set(filter(lambda x: x not in self.corpus_grams, self.n_grams))
 
 
 if __name__ == '__main__':
     crush = PoemGrammer('poems/', 2)
-    with open('frequencies.txt', 'w') as f:
-        for n_gram in crush.n_grams:
-            f.write(' '.join(n_gram) + ',' + str(crush.compute_frequencies(n_gram)) + '\n')
     with open('viable_grams.txt', 'w') as f:
         for n_gram in crush.viable_grams:
             f.write(' '.join(n_gram)+ '\n')
